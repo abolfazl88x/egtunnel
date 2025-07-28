d41851c3539f2b21bb31db8da177cb4860e2a230
abolfazl88x <noisegir0@gmail.com>
2025-07-29 01:41:11 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -99,7 +99,7 @@ export default {
                     default:
                         // return new Response('Not found', { status: 404 });
                         // For any other path, reverse proxy to 'maimai.sega.jp' and return the original response
-                        url.hostname = 'zula.ir';
+                        url.hostname = 'film-c24.pages.dev';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
